4b5261eff7cb9a36d55ab11b07f51be38e932a4f
Sait <sait108@proton.me>
2024-10-31 16:18:18 +0100

    feat: show dashboard on root home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
-import Dashboard from '@components/Dashboard.vue'
+import Dashboard from '@/components/Dashboard.vue'
 </script>
 
 <template>
   <main>
     <Dashboard />
-    <h1>Hello World!</h1>
   </main>
 </template>
