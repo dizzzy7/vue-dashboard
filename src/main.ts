8292e747371c6607a92250cea415fb5b8d47432c
Sait <sait108@proton.me>
2024-11-03 04:23:29 +0100

    chore: add tanstack query to project

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { VueQueryPlugin } from '@tanstack/vue-query'
 
 import '@/assets/_global.scss'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(VueQueryPlugin)
 
 app.mount('#app')
