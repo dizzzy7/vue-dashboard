d4a3eea4416461d147fb860fb447f7aeaf5e2244
Sait <sait108@proton.me>
2024-11-03 03:07:31 +0100

    feat: improve styling and responsiveness of dashboard

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -3,26 +3,24 @@
 
 <template>
   <div class="dashboard">
-    <div class="dashboard__item--span-one">Small item 1</div>
-    <div class="dashboard__item--span-one">Small item 2</div>
-    <div class="dashboard__item--span-one">Small item 3</div>
-    <div class="dashboard__item--span-three">3 Span Item</div>
-    <div class="dashboard__item--span-two">2 Span Item</div>
+    <div class="dashboard__item dashboard__item--span-one">Small item 1</div>
+    <div class="dashboard__item dashboard__item--span-one">Small item 2</div>
+    <div class="dashboard__item dashboard__item--span-one">Small item 3</div>
+    <div class="dashboard__item dashboard__item--span-three">3 Span Item</div>
+    <div class="dashboard__item dashboard__item--span-two">2 Span Item</div>
   </div>
 </template>
 
 <style scoped lang="scss">
 .dashboard {
-  background: lightblue;
+  background: #58546d;
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-columns: repeat(3, minmax(0, 1fr));
   padding: 1em;
   gap: 0.5em;
-
-  > * {
-    background-color: red;
-    border-radius: 0.4em;
-  }
+  grid-auto-flow: row;
+  justify-content: center;
+  border-radius: 1em;
 
   @media screen and (max-width: $xl-screen) {
   }
@@ -31,13 +29,20 @@
   }
 
   @media screen and (max-width: $md-screen) {
-    grid-template-columns: 1fr 1fr;
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+  }
+
+  @media screen and (max-width: $sm-screen) {
   }
 
   &__item {
-    @at-root [class*='#{&}'] {
-      background-color: green !important;
-    }
+    border-radius: 0.7em;
+    background-color: #7e77a1;
+    padding: 0.5em 1em;
+    min-height: 4em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
     &--span-one {
       grid-column-start: span 1;
@@ -47,6 +52,19 @@
     }
     &--span-three {
       grid-column-start: span 3;
+    }
+
+    @media screen and (max-width: $md-screen) {
+      &--span-two {
+        grid-column-start: span 2;
+      }
+
+      &--span-three {
+        grid-column-start: span 2;
+      }
+    }
+
+    @media screen and (max-width: $sm-screen) {
     }
   }
 }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,17 @@ import Dashboard from '@/components/Dashboard.vue'
 
 <template>
   <main>
-    <Dashboard />
+    <div class="container">
+      <section>
+        <h1 class="text-center">Dashboard</h1>
+        <Dashboard />
+      </section>
+    </div>
   </main>
 </template>
+
+<style scoped lang="scss">
+h1 {
+  margin-bottom: 0.6em;
+}
+</style>
