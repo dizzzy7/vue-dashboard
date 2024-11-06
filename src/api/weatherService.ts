03637d2df32533cbebf811bfb1c9a3b7cf8e2318
Sait <sait108@proton.me>
2024-11-06 23:58:53 +0100

    feat: remove getDailyWeather function and improve useWeather hook

--- src/api/weatherService.ts
+++ src/api/weatherService.ts
@@ -1,24 +1,11 @@
 import { useQuery } from '@tanstack/vue-query'
 import type { ComputedRef } from 'vue'
 
-export const getDailyWeather = async (lat: number, lon: number) => {
-  const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
-
-  const response = await fetch(
-    `http://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&forecast_days=1&timezone=${timeZone}&hourly=rain,temperature_2m`,
-  )
-
-  if (!response.ok) {
-    throw new Error(response.statusText)
-  }
-
-  return response.json()
-}
-
 export const useWeather = (
   location: ComputedRef<{ lat?: number; lon?: number }>,
   enabled: ComputedRef<boolean>,
   interval: 'hourly' | 'current', // could also accept array/set of 'hourly'/'current', to request both
+  forecastDays: number = 1,
 ) =>
   useQuery({
     queryKey: ['weather', interval],
@@ -26,7 +13,7 @@ export const useWeather = (
       const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
 
       const response = await fetch(
-        `http://api.open-meteo.com/v1/forecast?latitude=${location.value.lat}&longitude=${location.value.lon}&timezone=${timeZone}&${interval}=rain,temperature_2m`,
+        `http://api.open-meteo.com/v1/forecast?latitude=${location.value.lat}&longitude=${location.value.lon}&forecast_days=${forecastDays}&timezone=${timeZone}&${interval}=precipitation_probability,temperature_2m`,
       )
 
       if (!response.ok) {
